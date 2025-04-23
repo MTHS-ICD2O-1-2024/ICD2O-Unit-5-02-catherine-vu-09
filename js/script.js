efe4db504351326368ccc5717b6103744181e6f8
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-04-23 22:49:15 +0000

    added html style css icons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine
+// Created on: April 23 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+const positiveRandomNumber = Math.floor(Math.random() * 6) + 1
+const negativeRandomNumber = Math.floor(Math.random() * 6) * -1
+
+function generateNumber() {
+  // input
+  const userInput = document.querySelector('input[name="kind-of-number"]:checked').value;
+  // process
+  if (userInput === "1") {
+    //output
+    document.getElementById('result').innerHTML =
+      '<p>The random number is:' + positiveRandomNumber + '</p>'
+  }
+  else {
+    // output
+    document.getElementById('result').innerHTML =
+      '<p>The random number is: ' + negativeRandomNumber + '</p>';
+  }
+}
