deb3a9efad96d70ac28751dc4c4a9925a487a96f
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-05-31 23:29:51 +0000

    edit js

--- js/script.js
+++ js/script.js
@@ -4,27 +4,26 @@
 // Created on: April 23 2025
 // This file contains the JS functions for index.html
 
-'use strict'
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function generates random positive or negative number
  */
 // eslint-disable-next-line no-unused-vars
-const positiveRandomNumber = Math.floor(Math.random() * 6) + 1
-const negativeRandomNumber = Math.floor(Math.random() * 6) * -1
-
 function generateNumber() {
   // input
-  const userInput = document.querySelector('input[name="kind-of-number"]:checked').value;
+  const optionPositive = document.getElementById('option-positive').checked
+
   // process
-  if (userInput === "1") {
-    //output
-    document.getElementById('result').innerHTML =
-      '<p>The random number is:' + positiveRandomNumber + '</p>'
-  }
-  else {
+  if (optionPositive === true) {
     // output
     document.getElementById('result').innerHTML =
-      '<p>The random number is: ' + negativeRandomNumber + '</p>';
+      'Your random number is: ' + randomNumber
+  } else {
+    // process
+    const randomNegativeNumber = randomNumber * -1
+    // output
+    document.getElementById('result').innerHTML =
+      'Your random number is: ' + randomNegativeNumber
   }
 }
